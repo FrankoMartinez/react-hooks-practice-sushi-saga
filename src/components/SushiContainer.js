6433088afdd50b961eb7ebe8d0f26d9d385863e3
Francisco <frankmartinez4050@gmail.com>
2022-02-07 17:22:01 -0600

    Completed assignment

--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi";
 
-function SushiContainer(props) {
+function SushiContainer({ sushis, handleEatSushi }) {
+  const [sushiIndex, setSushiIndex] = useState(0)
+
+  const sushiComponents = sushis
+  .slice(sushiIndex, sushiIndex + 4)
+  .map((sushi) => {
+    return (
+    <Sushi key={sushi.id} sushi={sushi} onEatSushi={handleEatSushi} />
+    )
+  })
+
+  function handleClickMore() {
+    // (sushiIndex + 4) % sushis.lenght
+    // is a way to wrap back around to the beginning of the array once we get to the end
+    // using the % (remainder) operator
+    setSushiIndex((sushiIndex) => (sushiIndex + 4) % sushis.length)
+  }
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
-      <MoreButton />
+      {sushiComponents}
+      <MoreButton onClickMore={handleClickMore} />
     </div>
   );
 }
